1854b2d3e51caf01fbe0a766616cd7a48157eaa3
Israel de la Barrera Lora <isra.lora99@gmail.com>
2020-03-04 14:40:26 +0100

    no message

--- 0025_ProyectoRecursividad/src/ejercicio/Recursividad.java
+++ 0025_ProyectoRecursividad/src/ejercicio/Recursividad.java
@@ -43,15 +43,15 @@ public class Recursividad {
 	}
 	
 	private static boolean buscarArray(ArrayList<Object> arraylist, Object obj, int i) {
-		boolean status = false;
 			
 		if(i < arraylist.size() && !arraylist.contains(obj)) {
 			buscarArray(arraylist, obj, i+1);
-		} else if(arraylist.contains(obj)) {
-			status = true;
-		}
+		} else if(arraylist.equals(obj)) {
+			return true;
+		} 
 		
-		return status;
+		return false;
+		
 	}
 	
 	public static boolean buscarArray(ArrayList<Object> arraylist, Object obj) {
@@ -59,21 +59,110 @@ public class Recursividad {
 		}
 	
 	private static boolean buscarSustituirArray(ArrayList<Object> arraylista, Object buscar, Object sustituir, int i) {
-		boolean status = false;
-		if(i < arraylista.size() && !arraylista.contains(buscar)) {
-			buscarSustituirArray(arraylista, buscar, sustituir, i+1);
-		} else if(arraylista.contains(buscar)) {
-			arraylista.remove(i);
-			arraylista.add(i, sustituir);
-			status = true;	
+		if(i == arraylista.size()) {
+			return false;
+		} else if(arraylista.get(i).equals(buscar)) {
+			arraylista.set(i,sustituir);
+			return buscarSustituirArray(arraylista, buscar, sustituir, i+1) || true;	
+		} else {
+			return buscarSustituirArray(arraylista, buscar, sustituir, i+1);
 		}
-		
-		return status;
+	
 	}
 	
 	public static boolean buscarSustituirArray(ArrayList<Object> arrayLista, Object buscar, Object sustituir) {
 		return buscarSustituirArray(arrayLista, buscar, sustituir, 0);
 	}
 	
+	private static boolean todosEquals(ArrayList<Object> arrayLista, Object buscar, int i) {
+		if (i == arrayLista.size()) {
+			return true;
+		} else if(arrayLista.get(i).equals(buscar)) {
+			return todosEquals(arrayLista, buscar, i+1) && true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static boolean todosEquals(ArrayList<Object> arrayLista, Object buscar) {
+		return todosEquals(arrayLista, buscar, 0);
+	}
+	
+	public static void corridaArrayLista(ArrayList<Object> arrayLista) {
+		if(arrayLista.size() != 0) {
+			System.out.println(arrayLista.get(0));
+			arrayLista.remove(0);
+			corridaArrayLista(arrayLista);
+		} else {
+			
+		}
+	}
+	
+	public static void corridaArrayListaInt(ArrayList<Integer> arrayLista) {
+		if(arrayLista.size() != 0) {
+			System.out.println(arrayLista.get(0));
+			arrayLista.remove(0);
+			corridaArrayListaInt(arrayLista);
+		} else {
+			
+		}
+	}
+	
+	private static int sumaRecursiva(ArrayList<Integer> arrayListaInt, int i) {
+		if (i < arrayListaInt.size()) {
+			return arrayListaInt.get(i) + sumaRecursiva(arrayListaInt,i+1);
+		} else {
+			return 0;
+		}
+	}
+	
+	public static int sumaRecursiva(ArrayList<Integer> arrayListaInt) {
+		return sumaRecursiva(arrayListaInt, 0);
+	}
+	
+	private static int mediaRecursiva(ArrayList<Integer> arrayListaInt, int i) {
+		if(i == arrayListaInt.size()) {
+			return 0;
+		} else if(i == 0) {
+			return (arrayListaInt.get(i) + mediaRecursiva(arrayListaInt,i+1))/arrayListaInt.size();
+		} else {
+			return arrayListaInt.get(i) + mediaRecursiva(arrayListaInt,i+1);
+		}
+
+		
+	}
+	
+	public static int mediaRecursiva(ArrayList<Integer> arrayListaInt) {
+		return mediaRecursiva(arrayListaInt, 0);
+	}
+	
+	private static ArrayList<Integer> positivos(ArrayList<Integer> arrayListaInt, int i){	
+		if(i == arrayListaInt.size()) {
+			return new ArrayList<Integer>();
+		} else if(arrayListaInt.get(i) > 0) {
+			ArrayList<Integer> listaP = positivos(arrayListaInt,i+1);
+			listaP.add(arrayListaInt.get(i));
+			return listaP;
+		} else {
+			return positivos(arrayListaInt, i+1);
+		}
+		
+	}
+	
+	public static ArrayList<Integer> positivos(ArrayList<Integer> arrayListaInt){
+		return positivos(arrayListaInt, 0);
+	}
+	
+	private static void randomList(ArrayList<Integer> lista,int size, int i){
+		if (lista.size() == 0) {
+			
+		}
+	}
+	
+	public static ArrayList<Integer> randomList(int size){
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		randomList(lista,size, 0);
+		return lista;
+	}
 	
 }
--- 0025_ProyectoRecursividad/src/ejercicio/TestRecursividad.java
+++ 0025_ProyectoRecursividad/src/ejercicio/TestRecursividad.java
@@ -11,8 +11,16 @@ public class TestRecursividad {
 		arraylista.add(new String("hola1"));
 		arraylista.add(new String("hola2"));
 		arraylista.add(new String("hola3"));
-		System.out.println(Recursividad.buscarSustituirArray(arraylista, "hola3", "Que tal"));
-		Recursividad.arrayListCorrida(arraylista);
+		System.out.println(Recursividad.todosEquals(arraylista, "hola"));
+		Recursividad.corridaArrayLista(arraylista);
+		
+		ArrayList<Integer> arrayListaInt = new ArrayList<Integer>();
+		arrayListaInt.add(40);
+		arrayListaInt.add(-30);
+		arrayListaInt.add(20);
+		arrayListaInt.add(-50);
+		arrayListaInt.add(10);
+		Recursividad.corridaArrayListaInt(Recursividad.positivos(arrayListaInt));
 		
 	}
 
